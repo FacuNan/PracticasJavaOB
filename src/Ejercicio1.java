3c3c7a7559d3590d5e9b09ae1162d495f4682f5c
Facundo Comercio <facundocomercio@outlook.com>
2023-01-19 02:43:19 -0300

    tipos de datos open bootcamp

---
+++ src/Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+       int numeroEntero = 0;
+        System.out.println("numeroentero");
+        double numeroDecimal = 0.20;
+        System.out.println(numeroDecimal);
+        long telefono = 234567;
+        System.out.println(telefono);
+        boolean clasificado = true;
+        System.out.println(clasificado);
+        String saludo= " Mi nombre es Facundo y es un placer estar en OpenBootcamp";
+        System.out.println(saludo);
+    }
+}
