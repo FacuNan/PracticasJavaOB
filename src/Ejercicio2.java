50fc550f0393837c1bb4f73ff32635109326dc33
Facundo Comercio <facundocomercio@outlook.com>
2023-01-19 13:13:49 -0300

    Ejercio 2

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -7,15 +7,29 @@ public class Ejercicio2 {
         System.out.println("ingrese precio");
         int precio = scanner.nextInt();
         System.out.println("Ingrese iva");
-        double iva =scanner.nextDouble();
+        double iva = scanner.nextDouble();
 
-        double precioI = precioIva(precio, iva);
+        double precioI = precioConIva(precio, iva);
 
-        System.out.println("Su precio total con iva es " + precioI);
 
-        }
-    public static double precioIva(int precio, double iva) {
-return precio * iva;
+        System.out.println("Su precio con iva es  " + precioI);
 
     }
+
+    public static double calcularIva(int precio, double iva) {
+        return precio * iva;
+    }
+
+    public static double precioConIva(int precio, double iva){
+        double ivaCalculado = calcularIva(precio, iva);
+        return precio + ivaCalculado;
+
+
+    }
+
+
+
+
+
+
 }
